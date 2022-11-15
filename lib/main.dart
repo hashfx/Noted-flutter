c5b6857b017daf5b17d986e574df49b85207669c
hashfx <algebra.soni@gmail.com>
2022-11-15 22:00:24 +0530

    init flutter app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:noted/pages/home_page.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized(); // initialise app
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
