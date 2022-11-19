7a4a858bc545bc82d7460a038f042df9b2749300
hashfx <algebra.soni@gmail.com>
2022-11-19 19:30:40 +0530

    save data to firestore db

--- lib/pages/note_editor.dart
+++ lib/pages/note_editor.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:noted/style/app_style.dart';
 
@@ -25,11 +26,15 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
       appBar: AppBar(
         backgroundColor: AppStyle.cardsColor[color_id],
         elevation: 0.0,
-        title: Text("Add a new Note"),
+        iconTheme: IconThemeData(color: Colors.black),
+        title: const Text(
+          "Add a new Note",
+          style: TextStyle(color: Colors.black),
+        ),
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Column(children: [
+        child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
           TextField(
             controller: _titleController,
             decoration: InputDecoration(
@@ -44,7 +49,6 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
             style: AppStyle.mainTitle,
           ),
           SizedBox(height: 28.0),
-
           TextField(
             controller: _mainController,
             keyboardType: TextInputType.multiline,
@@ -56,6 +60,22 @@ class _NoteEditorScreenState extends State<NoteEditorScreen> {
             style: AppStyle.mainContent,
           ),
         ]),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: AppStyle.accentColor,
+        onPressed: () async {
+          FirebaseFirestore.instance.collection("Notes").add({
+            "note_title": _titleController.text,
+            "creation_date": date,
+            "note_content": _mainController.text,
+            "color_id": color_id
+          }).then((value) {
+            print(value.id);
+            Navigator.pop(context);
+          }).catchError(
+              (error) => print("Failed to add new note error: $error"));
+        },
+        child: Icon(Icons.save),
       ),
     );
   }
